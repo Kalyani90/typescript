16e7ed92dd3ab83491e2853f24a9c9549d5bfc05
Kalyani90 <kalyani.chutepatil@gmail.com>
2017-09-08 20:04:08 +0530

    second

---
+++ 4 day/1abc.ts
@@ -0,0 +1,18 @@
+
+ import { Files, PI } from "./files";
+
+
+class Computer {
+    private files: Files;
+
+    constructor() {
+        console.log(PI);
+    }
+
+    reSizeIt(sz?: number) {
+
+    }
+}
+
+let com : Computer = new Computer();
+com.reSizeIt();
